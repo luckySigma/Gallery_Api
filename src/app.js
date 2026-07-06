07b71b74bfa03c906bf374ce6dd241e18e5c96a9
luckySigma <adsanjeewa37@gmail.com>
2026-07-06 21:18:41 +0530

    Feature/cors (#3)
    * feat: update image controller,configure CORS, and update gitignore
    
    * feat: update image controller,configure CORS, and update gitignore

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
-require("dotenv").config()
+require("dotenv").config();
+
+const cors = require("cors");
 const express = require("express");
 const imageRoutes = require("./routes/image.routes");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "https://image-gallery-xer4hohs5-anuradha7.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
 
 app.use("/api/images", imageRoutes);
 
